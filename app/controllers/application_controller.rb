1ee0dd38e7726321c82eda65fc4fb3d00978836e
toyomaho <151604091+toyomaho@users.noreply.github.com>
2023-12-05 18:17:23 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+end
+
+private
+
+def basic_auth
+  authenticate_or_request_with_http_basic do |username, password|
+    # 環境変数を読み込む記述に変更
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  end
 end
